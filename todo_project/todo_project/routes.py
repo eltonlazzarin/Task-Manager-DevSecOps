1996f72f424b85ae8d0c57b0ce40b880e571947a
Elton Lazzarin <elton.lazzarin@outlook.com>
2026-06-10 01:30:17 -0300

    Adds logging for error events and user actions on routes

--- todo_project/todo_project/routes.py
+++ todo_project/todo_project/routes.py
@@ -1,28 +1,56 @@
 from flask import render_template, url_for, flash, redirect, request
 
-from todo_project import app, db, bcrypt
+from todo_project import app, db, bcrypt, logger
 
 # Import the forms
-from todo_project.forms import (LoginForm, RegistrationForm, UpdateUserInfoForm, 
-                                UpdateUserPassword, TaskForm, UpdateTaskForm)
+from todo_project.forms import (
+    LoginForm,
+    RegistrationForm,
+    UpdateUserInfoForm,
+    UpdateUserPassword,
+    TaskForm,
+    UpdateTaskForm
+)
 
 # Import the Models
 from todo_project.models import User, Task
 
-# Import 
-from flask_login import login_required, current_user, login_user, logout_user
+# Import
+from flask_login import (
+    login_required,
+    current_user,
+    login_user,
+    logout_user
+)
 
 
 @app.errorhandler(404)
 def error_404(error):
+
+    logger.warning(
+        f"ERRO_404 usuario={current_user.username if current_user.is_authenticated else 'anonimo'}"
+    )
+
     return (render_template('errors/404.html'), 404)
+
 
 @app.errorhandler(403)
 def error_403(error):
+
+    logger.warning(
+        f"ERRO_403 usuario={current_user.username if current_user.is_authenticated else 'anonimo'}"
+    )
+
     return (render_template('errors/403.html'), 403)
+
 
 @app.errorhandler(500)
 def error_500(error):
+
+    logger.error(
+        f"ERRO_500 usuario={current_user.username if current_user.is_authenticated else 'anonimo'}"
+    )
+
     return (render_template('errors/500.html'), 500)
 
 
@@ -34,125 +62,303 @@ def about():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+
     if current_user.is_authenticated:
         return redirect(url_for('all_tasks'))
 
     form = LoginForm()
-    # After you submit the form
+
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        # Check if the user exists and the password is valid
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+
+        user = User.query.filter_by(
+            username=form.username.data
+        ).first()
+
+        if user and bcrypt.check_password_hash(
+                user.password,
+                form.password.data):
+
             login_user(user)
-            task_form = TaskForm()
-            flash('Login Successfull', 'success')
+
+            logger.info(
+                f"LOGIN_SUCESSO usuario={user.username}"
+            )
+
+            flash(
+                'Login Successfull',
+                'success'
+            )
+
             return redirect(url_for('all_tasks'))
+
         else:
-            flash('Login Unsuccessful. Please check Username Or Password', 'danger')
-    
-    return render_template('login.html', title='Login', form=form)
-    
+
+            logger.warning(
+                f"LOGIN_FALHA usuario={form.username.data}"
+            )
+
+            flash(
+                'Login Unsuccessful. Please check Username Or Password',
+                'danger'
+            )
+
+    return render_template(
+        'login.html',
+        title='Login',
+        form=form
+    )
+
 
 @app.route("/logout")
+@login_required
 def logout():
+
+    logger.info(
+        f"LOGOUT usuario={current_user.username}"
+    )
+
     logout_user()
+
     return redirect(url_for('login'))
 
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
+
     if current_user.is_authenticated:
         return redirect(url_for('all_tasks'))
 
     form = RegistrationForm()
+
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, password=hashed_password)
+
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data
+        ).decode('utf-8')
+
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+
         db.session.add(user)
         db.session.commit()
-        flash(f'Account Created For {form.username.data}', 'success')
+
+        logger.info(
+            f"USUARIO_CRIADO usuario={form.username.data}"
+        )
+
+        flash(
+            f'Account Created For {form.username.data}',
+            'success'
+        )
+
         return redirect(url_for('login'))
 
-    return render_template('register.html', title='Register', form=form)
+    return render_template(
+        'register.html',
+        title='Register',
+        form=form
+    )
 
 
 @app.route("/all_tasks")
 @login_required
 def all_tasks():
-    tasks = User.query.filter_by(username=current_user.username).first().tasks
-    return render_template('all_tasks.html', title='All Tasks', tasks=tasks)
+
+    tasks = User.query.filter_by(
+        username=current_user.username
+    ).first().tasks
+
+    return render_template(
+        'all_tasks.html',
+        title='All Tasks',
+        tasks=tasks
+    )
 
 
 @app.route("/add_task", methods=['POST', 'GET'])
 @login_required
 def add_task():
+
     form = TaskForm()
+
     if form.validate_on_submit():
-        task = Task(content=form.task_name.data, author=current_user)
+
+        task = Task(
+            content=form.task_name.data,
+            author=current_user
+        )
+
         db.session.add(task)
         db.session.commit()
-        flash('Task Created', 'success')
+
+        logger.info(
+            f"TAREFA_CRIADA usuario={current_user.username}"
+        )
+
+        flash(
+            'Task Created',
+            'success'
+        )
+
         return redirect(url_for('add_task'))
-    return render_template('add_task.html', form=form, title='Add Task')
+
+    return render_template(
+        'add_task.html',
+        form=form,
+        title='Add Task'
+    )
 
 
-@app.route("/all_tasks/<int:task_id>/update_task", methods=['GET', 'POST'])
+@app.route("/all_tasks/<int:task_id>/update_task",
+           methods=['GET', 'POST'])
 @login_required
 def update_task(task_id):
+
     task = Task.query.get_or_404(task_id)
+
     form = UpdateTaskForm()
+
     if form.validate_on_submit():
+
         if form.task_name.data != task.content:
+
             task.content = form.task_name.data
+
             db.session.commit()
-            flash('Task Updated', 'success')
+
+            logger.info(
+                f"TAREFA_ATUALIZADA usuario={current_user.username} tarefa_id={task.id}"
+            )
+
+            flash(
+                'Task Updated',
+                'success'
+            )
+
             return redirect(url_for('all_tasks'))
+
         else:
-            flash('No Changes Made', 'warning')
+
+            flash(
+                'No Changes Made',
+                'warning'
+            )
+
             return redirect(url_for('all_tasks'))
+
     elif request.method == 'GET':
+
         form.task_name.data = task.content
-    return render_template('add_task.html', title='Update Task', form=form)
+
+    return render_template(
+        'add_task.html',
+        title='Update Task',
+        form=form
+    )
 
 
 @app.route("/all_tasks/<int:task_id>/delete_task")
 @login_required
 def delete_task(task_id):
+
     task = Task.query.get_or_404(task_id)
+
     db.session.delete(task)
     db.session.commit()
-    flash('Task Deleted', 'info')
+
+    logger.warning(
+        f"TAREFA_EXCLUIDA usuario={current_user.username} tarefa_id={task.id}"
+    )
+
+    flash(
+        'Task Deleted',
+        'info'
+    )
+
     return redirect(url_for('all_tasks'))
 
 
 @app.route("/account", methods=['POST', 'GET'])
 @login_required
 def account():
+
     form = UpdateUserInfoForm()
+
     if form.validate_on_submit():
-        if form.username.data != current_user.username:  
+
+        if form.username.data != current_user.username:
+
             current_user.username = form.username.data
+
             db.session.commit()
-            flash('Username Updated Successfully', 'success')
+
+            logger.info(
+                f"USUARIO_ALTERADO usuario={current_user.username}"
+            )
+
+            flash(
+                'Username Updated Successfully',
+                'success'
+            )
+
             return redirect(url_for('account'))
+
     elif request.method == 'GET':
-        form.username.data = current_user.username 
 
-    return render_template('account.html', title='Account Settings', form=form)
+        form.username.data = current_user.username
+
+    return render_template(
+        'account.html',
+        title='Account Settings',
+        form=form
+    )
 
 
-@app.route("/account/change_password", methods=['POST', 'GET'])
+@app.route("/account/change_password",
+           methods=['POST', 'GET'])
 @login_required
 def change_password():
+
     form = UpdateUserPassword()
+
     if form.validate_on_submit():
-        if bcrypt.check_password_hash(current_user.password, form.old_password.data):
-            current_user.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+
+        if bcrypt.check_password_hash(
+                current_user.password,
+                form.old_password.data):
+
+            current_user.password = bcrypt.generate_password_hash(
+                form.new_password.data
+            ).decode('utf-8')
+
             db.session.commit()
-            flash('Password Changed Successfully', 'success')
-            redirect(url_for('account'))
+
+            logger.warning(
+                f"SENHA_ALTERADA usuario={current_user.username}"
+            )
+
+            flash(
+                'Password Changed Successfully',
+                'success'
+            )
+
+            return redirect(url_for('account'))
+
         else:
-            flash('Please Enter Correct Password', 'danger') 
 
-    return render_template('change_password.html', title='Change Password', form=form)
+            logger.warning(
+                f"TENTATIVA_ALTERACAO_SENHA_FALHA usuario={current_user.username}"
+            )
 
+            flash(
+                'Please Enter Correct Password',
+                'danger'
+            )
+
+    return render_template(
+        'change_password.html',
+        title='Change Password',
+        form=form
+    )
