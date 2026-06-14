d6d659bd159c083a2fb87002d2e58b1cb6723a86
Elton Lazzarin <elton.lazzarin@outlook.com>
2026-06-13 21:15:14 -0300

    Remove unnecessary comment in forms.py

--- todo_project/forms.py
+++ todo_project/forms.py
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
 # Import the User Database Model
 from todo_project.models import User
 
+# ll
 from flask_login import current_user
 
 
